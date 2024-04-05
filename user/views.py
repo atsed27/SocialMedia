9004785904887d97c4fc268e4cbd3f4927dd7199
Lap <you@example.com>
2024-04-05 17:37:25 -0300

    user login is doen

--- user/views.py
+++ user/views.py
@@ -3,8 +3,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import AuthenticationFailed
 from .serializers import UserSerializer
+from rest_framework import status
 from .models import User
-
+import jwt, datetime
 
 
 import bcrypt
@@ -33,4 +34,48 @@ class RegisterView(APIView):
         return Response(serializer.data)
 
 
+#Login class
 
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data["email"]
+        password = request.data["password"]
+        print(email, password)
+
+        # Find user by Email
+        findUser = User.objects.filter(email=email).first()
+
+        print(findUser is None)
+        print(findUser)
+
+        if findUser is None:
+            return Response("User is not found", status=status.HTTP_404_NOT_FOUND)
+
+        # Compare the provided password with the hashed password
+        checkPassword = bcrypt.checkpw(password.encode('utf-8'), findUser.password.encode('utf-8'))
+
+        print(checkPassword)
+
+        if not checkPassword:
+            return Response(
+                {"message": "Password is incorrect", "status": "409", "error": "True"},
+                status=status.HTTP_409_CONFLICT,
+            )
+
+        # Create Jwt Token
+        payload = {
+            'id': str(findUser.id),  # Convert UUID to string
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.utcnow()
+        }
+
+        token = jwt.encode(payload, 'secret', algorithm='HS256')
+
+        response = Response()
+        response.set_cookie(key='ene', value=token, httponly=True)
+        
+        # Serialize the user object
+        serializer = UserSerializer(findUser)
+        
+        response.data = serializer.data
+        return response
