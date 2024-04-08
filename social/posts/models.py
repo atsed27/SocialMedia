32ca8c23994b3bcb017abe408279972f4061861a
atsed27 <danielnigatu09@gmail.com>
2024-04-08 11:31:35 -0300

    docker postgresql

--- social/posts/models.py
+++ social/posts/models.py
@@ -14,3 +14,12 @@ class  Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     user = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
+    
+    
+    
+class PostLike(models.Model):
+    post = models.ForeignKey(Post, null=False, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = (("post", "user"), )
