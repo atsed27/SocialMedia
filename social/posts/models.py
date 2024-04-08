7a953dad6b1809bd48e24d5c8aa98a26de6c5bb1
atsed27 <danielnigatu09@gmail.com>
2024-04-08 06:32:54 -0300

    update

--- social/posts/models.py
+++ social/posts/models.py
@@ -2,13 +2,13 @@ from django.db import models
 from user.models import User
 
 
-
+import uuid
 
 
 # Create your models here.
 
 class  Post(models.Model):
-    
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=32)
     description = models.CharField(max_length=164)
     created_at = models.DateTimeField(auto_now_add=True)
--- social/posts/urls.py
+++ social/posts/urls.py
@@ -7,7 +7,6 @@ from django.urls import re_path
 
 urlpatterns = [
     path('create/', csrf_exempt(VerifyToken(Create.as_view()))),
-   # path('update/<int:pk>/' ,csrf_exempt(VerifyToken(Update.as_view()))),
-    path('update/<int:pk>/', csrf_exempt(VerifyToken(Update.as_view())))
+    re_path(r'^update/(?P<pk>[0-9a-f-]+)/$', csrf_exempt(VerifyToken(Update.as_view())), name='update_post'),
 
  ]
