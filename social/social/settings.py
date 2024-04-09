204a3d8100f1c833e1f909b0e8ae89d3862ea3ae
atsed27 <danielnigatu09@gmail.com>
2024-04-09 05:26:52 -0300

    set up firabase

--- social/social/settings.py
+++ social/social/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 from pathlib import Path
 import dotenv
 dotenv.load_dotenv()
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,6 +142,7 @@ CORS_ALLOW_CREDENTIALS = True
 
 
 # Firebase Storage settings
+d=os.getenv('GS_CREDENTIALS')
 
-
+print(d)
 
